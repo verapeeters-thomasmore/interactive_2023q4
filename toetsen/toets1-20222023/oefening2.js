b1468893c7d84e854247da401ea62b09697a29a4
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2023-05-09 11:42:44 +0200

    toets1

---
+++ toetsen/toets1-20222023/oefening2.js
@@ -0,0 +1,12 @@
+const pricePerParticipant = (participants, totalCostForWorkshop, minParticipants, maxParticipants) => {
+    return participants < minParticipants ? -1 : Math.max(totalCostForWorkshop / Math.min(participants, maxParticipants), 20);
+};
+
+console.log(pricePerParticipant(10, 1000, 1, 20)); //100
+console.log(pricePerParticipant(5, 1000, 1, 20)); //20
+console.log(pricePerParticipant(20, 700, 1, 20)); //35
+console.log(pricePerParticipant(10, 100, 1, 20)); //20 because minimum price is 20
+console.log(pricePerParticipant(20, 1000, 1, 10)); //100 because no more than 10 participants
+console.log(pricePerParticipant(20, 1000, 1, 5)); //200 because no more than 5 participants
+console.log(pricePerParticipant(10, 1000, 20, 5)); //-1 because less then 20 participants
+
---
+++ toetsen/toets1-20222023/oefening3.js
@@ -0,0 +1,17 @@
+function pricePerParticipant(participants, totalCostForWorkshop, minParticipants=1, maxParticipants=20) {
+    if (participants < minParticipants) return -1;
+    return Math.max(totalCostForWorkshop / Math.min(participants, maxParticipants), 20);
+}
+
+console.log(pricePerParticipant(10, 1000, 1, 20)); //100
+console.log(pricePerParticipant(5, 1000, 1, 20)); //20
+console.log(pricePerParticipant(20, 700, 1, 20)); //35
+console.log(pricePerParticipant(10, 100, 1, 20)); //20 because minimum price is 20
+console.log(pricePerParticipant(20, 1000, 1, 10)); //100 because no more than 10 participants
+console.log(pricePerParticipant(20, 1000, 1, 5)); //200 because no more than 5 participants
+console.log(pricePerParticipant(10, 1000, 20, 5)); //-1 because less then 20 participants
+
+console.log("extra gevallen: ")
+console.log(pricePerParticipant(10, 1000)); //100
+console.log(pricePerParticipant(20, 1000, undefined, 10)); //100 because no more than 10 participants
+console.log(pricePerParticipant(10, 1000, 20)); //-1 because less then 20 participants
