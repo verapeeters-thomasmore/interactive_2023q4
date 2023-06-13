135ce782cecd2407468d0343f5d29ebe30a93e07
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2023-06-13 14:03:46 +0200

    toets herkansing start code

---
+++ toetsen/toets-herkansing-20222023/oefening1.js
@@ -0,0 +1,45 @@
+const fietstochten = [
+    {
+        fiets: "MTB",
+        aantalKm: 89,
+        gemiddeldeSnelheid: 21,
+        fietsvriendinnen: ["Anne", "Mieke", "Lea"],
+        bestemming: "Scherpenheuvel"
+    },
+    {
+        fiets: "MTB",
+        aantalKm: 52,
+        gemiddeldeSnelheid: 23,
+        fietsvriendinnen: ["Anne", "Mieke", "Elke", "Vera"],
+        bestemming: "Holsbeek"
+    },
+    {
+        fiets: "MTB",
+        aantalKm: 36,
+        gemiddeldeSnelheid: 24,
+        fietsvriendinnen: ["Jozefien", "Mieke", "Anne", "Lieve"],
+        bestemming: "Lier"
+    },
+    {
+        fiets: "koersfiets",
+        aantalKm: 136,
+        gemiddeldeSnelheid: 24,
+        fietsvriendinnen: ["Anne", "Elke", "Lieve", "Mieke", "Karolien"],
+        bestemming: "Oostende"
+    },
+    {
+        fiets: "koersfiets",
+        aantalKm: 100,
+        gemiddeldeSnelheid: 24,
+        fietsvriendinnen: ["Mieke", "Anne", "Karolien", "Lieve", "Elke", "Bea"],
+        bestemming: "Voeren"
+    },
+    {
+        fiets: "koersfiets",
+        aantalKm: 60,
+        gemiddeldeSnelheid: 26,
+        fietsvriendinnen: ["Bea", "Mieke", "Anne"],
+        bestemming: "Lier"
+    }
+]
+
