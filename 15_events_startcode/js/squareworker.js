817fd8cdf0ba4c9f5072dce3b42d80beaa96c60d
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2024-04-22 18:46:43 +0200

    remove all var's

--- 15_events_startcode/js/squareworker.js
+++ 15_events_startcode/js/squareworker.js
@@ -1,7 +1,7 @@
 addEventListener("message", event => {
     console.log(event);
     // imiteer een lange bewerking in het worker script
-    var currentTime = new Date().getTime();
+    let currentTime = new Date().getTime();
     while (currentTime + 10000 >= new Date().getTime()) {
     }
     postMessage(event.data * event.data);
