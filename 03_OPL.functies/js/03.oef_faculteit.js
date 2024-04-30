bee0d3b697809f4ad55628d2ef3060f6c0d5aaff
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2024-04-30 12:12:29 +0200

    verschillende versies van de oefeningen faculteit en xdemacht

--- 03_OPL.functies/js/03.oef_faculteit.js
+++ 03_OPL.functies/js/03.oef_faculteit.js
@@ -1,10 +1,35 @@
 const faculteit = (basis) => {
     let result = 1;
-    for (let count = 2; count <= basis; count++) {
+    for (let count = 1; count <= basis; count++) {
         result *= count;
     }
     return result;
 }
 console.log(faculteit(1));
+console.log(faculteit(6));
+console.log(faculteit(8));
+
+//kortere versie - met recursie - dus zonder let
+const faculteit2 = basis => {
+    return basis <= 2 ? basis : basis * faculteit2(basis - 1);
+};
+
+console.log(faculteit2(-1)); //-1
+console.log(faculteit2(1)); //1
+console.log(faculteit2(2)); //2
+console.log(faculteit2(3)); //3 * faculteit2(2) = 3 * 2 = 6
+console.log(faculteit2(4)); //4 * faculteit2(3) = 4 * (3 * faculteit2(2)) = 4 * (3 * 2) = 24
+console.log(faculteit2(6));
+console.log(faculteit2(8));
 
 
+//kortere versie - met recursie en korte notatie van arrow functie
+const faculteit3 = basis => basis <= 2 ? basis : basis * faculteit3(basis - 1);
+
+console.log(faculteit3(-1)); //-1
+console.log(faculteit3(1)); //1
+console.log(faculteit3(2)); //2
+console.log(faculteit3(3)); //3 * faculteit2(2) = 3 * 2 = 6
+console.log(faculteit3(4)); //4 * faculteit2(3) = 4 * (3 * faculteit2(2)) = 4 * (3 * 2) = 24
+console.log(faculteit3(6));
+console.log(faculteit3(8));
--- 03_OPL.functies/js/04.oef_tweedeDerdeMacht.js
+++ 03_OPL.functies/js/04.oef_tweedeDerdeMacht.js
@@ -1,11 +1,42 @@
+//exponent is 2 of 3 (afspraak)
+//  merk op - we testen hier niet of de parameter juist is,
+//  dus als bvb parameter exponent 4 is krijg je een niet-intuitief resultaat)
+//function-syntax
+const tweedeDerdeMacht_v1 = function (number, exponent) {
+    if (exponent === 2) {
+        return number * number;
+    } else {
+        return number * number * number;
+    }
+};
+
+
 //exponent is 2 of 3
+//arrow-function-syntax
 const tweedeDerdeMacht = (number, exponent) => {
-    return (exponent === 2) ? number * number : number * number * number;
-}
+    if (exponent === 2) {
+        return number * number;
+    } else {
+        return number * number * number;
+    }
+};
 
 console.log(tweedeDerdeMacht(4, 2)); // 4*4 = 16
 console.log(tweedeDerdeMacht(4, 3)); // 4*4*4 = 64
 
+//exponent is 2 of 3
+//arrow-function-syntax, kortere notatie
+const tweedeDerdeMacht_v2 = (number, exponent) => {
+    return exponent === 2 ? number * number : number * number * number;
+};
+
+
+//exponent is 2 of 3
+//arrow-function-syntax, nog kortere notatie
+const tweedeDerdeMacht_v3 = (number, exponent) => exponent === 2 ? number * number : number * number * number;
+
+//===============================================
+//exponent mag eender welke waarde zijn
 const xDeMacht_v1 = (number, exponent) => {
     let result = 1;
     for (let i = 0; i < exponent; i++)
@@ -13,12 +44,23 @@ const xDeMacht_v1 = (number, exponent) => {
     return result;
 }
 
-
 //exponent is eender wat
+//kortere versie met recursie
 //                  4       2
-const xDeMacht = (number, exponent) => {
+const xDeMacht_v2 = (number, exponent) => {
     return (exponent === 0) ? 1 : number * xDeMacht(number, exponent - 1);
 }
+
+
+//exponent is eender wat
+//kortere versie - ken je taal!!!!!!!!!
+const xDeMacht_v3 = (number, exponent) => {
+    return number ** exponent;
+}
+
+//exponent is eender wat
+//kortere versie - kmet korte notatie
+const xDeMacht = (number, exponent) => number ** exponent;
 
 console.log("xDeMacht")
 console.log(xDeMacht(4, 0)); // 1
@@ -26,3 +68,5 @@ console.log(xDeMacht(4, 1)); // 4 = 4
 console.log(xDeMacht(4, 2)); // 4*4 = 16
 console.log(xDeMacht(4, 3)); // 4*4*4 = 64
 console.log(xDeMacht(4, 4)); // 4**4 = 4*4*4*4 = 256
+
+
