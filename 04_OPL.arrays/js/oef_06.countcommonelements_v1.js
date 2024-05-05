f22d63db60ae9e906ff3a466b52ee13739dea2a7
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2024-05-05 12:00:42 +0200

    startcode

--- 04_OPL.arrays/js/oef_06.countcommonelements_v1.js
+++ 04_OPL.arrays/js/oef_06.countcommonelements_v1.js
@@ -1,22 +1,24 @@
-
 function countCommonElements(array1, array2) {
-    const gevondenPlaatsen = []
-    for (i = 0; i < array1.length; i++) {
-        for (j = 0; j < array2.length; j++) {
-            if (array1[i] === array2[j] && !gevondenPlaatsen.includes(j)) {
-                gevondenPlaatsen.push(j);
-            }
+    let foundIndexesInArray2 = [];
+    array1.forEach(el1 => {
+            array2.forEach((el2, index2) => {
+                    if (el1 === el2 && !foundIndexesInArray2.includes(index2)) {
+                        foundIndexesInArray2 = foundIndexesInArray2.concat([index2]);
+                    }
+                }
+            )
         }
-    }
-    return gevondenPlaatsen.length;
+    )
+    return foundIndexesInArray2.length;
 }
-
 
 console.log(countCommonElements([], [])); //0
 console.log(countCommonElements([1], [1])); //1
 console.log(countCommonElements([1, 2], [1, 2])); //2
+console.log(countCommonElements([2, 1], [1, 2])); //2
 console.log(countCommonElements([1, 2], [1, 3])); //1
 console.log(countCommonElements([1, 2, 2], [1, 2])); //2
+console.log(countCommonElements([2, 4, 1, 2], [1, 4])); //2
 console.log(countCommonElements([1, 2, 2], [2, 1, 2])); //3
 
 console.log(countCommonElements([1, 2, 5, 5, 8, 9, 7, 10], [1, 0, 6, 15, 6, 4, 7, 0])); //2
