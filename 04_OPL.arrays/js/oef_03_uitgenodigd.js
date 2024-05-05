5eeb1fccac0613eb84d6c52182bcc9b51b4346fe
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2024-05-05 10:38:46 +0200

    oefening 3 - copy solution to OPL folder

--- 04_OPL.arrays/js/oef_03_uitgenodigd.js
+++ 04_OPL.arrays/js/oef_03_uitgenodigd.js
@@ -1,40 +1,73 @@
-const visitors = ["Jan", "Badr", "Jos", "Mieke", "Anna", "Fien", "Klara", "Souf"];
-const resultEl = document.getElementById("result");
+const messageEl = document.getElementById("message");
+const visitorListEl = document.getElementById("visitorList");
 const nameEl = document.getElementById("name");
 
+//state of the application: what changes on user actions
+//dit verandert - dus let!
+let visitors = ["Jan", "Badr", "Jos", "Mieke", "Anna", "Fien", "Klara", "Souf"];
+let showVisitors = false;
 
+//puur functionele check - geen side-effects
 function isInvited(name) {
-    return visitors.map(visitor => visitor.toLowerCase()).includes(name.toLowerCase());
+    return visitors.find(v => v.toLowerCase() === name.toLowerCase());
 }
 
-function checkInvited() {
-    resultEl.innerHTML = !nameEl.value ? "vul een naam in" : isInvited(nameEl.value) ? "Welkom" : "Niet welkom";
+//aparte functie omdat we dan met return kunnen werken
+//UI-functie
+function getMessageCheckInvited(name) {
+    if (!name) return "vul een naam in.";
+
+    const invitedName = isInvited(name);
+    return invitedName ? `${invitedName} is welkom!` : `${invitedName} is niet uitgenodigd.`;
 }
 
-function showAllInvited() {
-    return resultEl.innerHTML = visitors
-        .map(entry => `<li>${entry}</li>`)
-        .join("");
+//UI-functie
+//toont "<name> is welkom!" of "<name> is niet uitgenodigd."
+//check alleen volledige naam, case-insensitive
+function checkVisitorPresentButton() {
+    messageEl.innerHTML = getMessageCheckInvited(nameEl.value);
 }
 
-function showVIP() {
-    return resultEl.innerHTML = visitors
-        .filter(entry => entry.toLowerCase().startsWith("j"))
-        .map(entry => `<div><b>${entry}</b> is een VIP!!</div>`)
-        .join("");
+function showVisitorsInUI() {
+    if (showVisitors)
+        visitorListEl.innerHTML = visitors
+            .filter(entry => entry.toLowerCase().includes(nameEl.value.toLowerCase()))
+            .map(entry => `<li>${entry}</li>`)
+            .join("");
+    else
+        visitorListEl.innerHTML = "";
 }
 
-const pushVisitors = name => {
-    //TODO geen push
-    visitors.push(name);
-    console.log(visitors);
+//UI-functie
+function toggleShowVisitorsButton() {
+    showVisitors = !showVisitors;
+    showVisitorsInUI();
 }
 
-const addInvitee = () => {
-    const name = nameEl.value;
-    pushVisitors(name);
-    resultEl.innerHTML = name + " toegevoegd.";
+//functionele functie - maakt nieuwe array - dus geen side-effects
+function listWithExtraInvitee(name) {
+    return visitors.concat(name);
 }
 
-//en nu ook op console
-(visitors.filter(entry => entry.toLowerCase().startsWith("j"))).forEach(entry => console.log(entry))
+//functionele functie - returnt wel UI-message
+function addInvitee(name) {
+    //preconditions:
+    if (!name) return "vul een naam in.";
+    if (isInvited(name)) return `${name} was al uitgenodigd.`;
+
+    visitors = listWithExtraInvitee(name);
+    return `${name} toegevoegd.`;
+}
+
+//UI-functie
+//check first: name should not be empty and should not be in the list already
+//create a new array and assign it to the visitors variable
+function addVisitorButton() {
+    messageEl.innerHTML = addInvitee(nameEl.value);
+    nameEl.value = "";
+    filterVisitors();
+}
+
+function filterVisitors() {
+    showVisitorsInUI();
+}
