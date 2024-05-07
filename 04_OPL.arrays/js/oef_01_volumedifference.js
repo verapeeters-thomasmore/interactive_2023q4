ed1fa1d7f2fbea919c3f82537121adf821f61023
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2024-05-07 09:50:55 +0200

    more possibilities for oef 01

--- 04_OPL.arrays/js/oef_01_volumedifference.js
+++ 04_OPL.arrays/js/oef_01_volumedifference.js
@@ -2,6 +2,14 @@
 // de functie krijgt 1 paramater binnen = block
 //1 return statement => dus {} en return weg
 const volumeCalculation = block => block[0] * block[1] * block[2];
+const volumeCalculationV2 = function (block) {
+    return block[0] * block[1] * block[2];
+}
+const volumeCalculationV3 = function (block) {
+    let  volume = 1;
+    for (const el of block) volume *= el;
+    return volume;
+}
 
 console.log(`volume blok 2 : ${volumeCalculation([5, 8, 6])}`); //expected 240
 console.log(`volume blok 1 : ${volumeCalculation([5, 4, 3])}`); //expected 60
