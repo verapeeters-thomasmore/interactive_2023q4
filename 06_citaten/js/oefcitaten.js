a1a607bac85afc651518d3fcddabb864d80e24bd
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2024-05-20 11:48:16 +0200

    try to clean up solution code

--- 06_citaten/js/oefcitaten.js
+++ 06_citaten/js/oefcitaten.js
@@ -1,59 +1,69 @@
 const QUOTES = [
     {
+        id: 0,
         text: 'De dieren lijden en hun gejammer vervult de lucht. De bossen vallen ten prooi aan vernietiging. De bergen worden opengescheurd voor de metalen die in hun aderen groeien. En de mens looft en prijst degenen die aan de natuur en aan de mensheid de grootste schade berokkenen.',
         author: 'Leonardo da Vinci',
         info: 'Italiaans schilder, ingenieur en musicus',
         life: 'Leefde van: 1452-1519'
     },
     {
+        id: 1,
         text: 'Logica brengt je van A naar B. Verbeelding brengt je overal.',
         author: 'Albert Einstein',
         info: 'Duits -Amerikaans natuurkundige',
         life: '1879 - 1955'
     },
     {
+        id: 2,
         text: 'Geef me werk wat bij me past en ik hoef nooit meer te werken.',
         author: 'Confucius',
         info: 'Chinees filosoof',
         life: '551 v.C. - 479 v.C.'
     },
     {
+        id: 3,
         text: 'Mensen eisen vrijheid van meningsuiting als compensatie voor de vrijheid van denken waar ze zelden gebruik van maken.',
         author: 'Sören Kierkegaard',
         info: 'Deens filosoof',
         life: '1813-1855'
     },
     {
+        id: 4,
         text: 'Een uur zitten bij een aardig meisje vliegt voorbij als een minuut, maar een minuut op een brandende kachel lijkt wel een uur. Dat is relativiteit.',
         author: 'Albert Einstein',
         info: 'Duits - Amerikaans natuurkundige',
         life: '1879-1955'
     },
     {
+        id: 5,
         text: 'De moeilijkheid met de wereld is dat de dommen zelfverzekerd zijn en de verstandigen met twijfel vervuld.',
         author: 'Bertrand Russell',
         info: 'Engels filosoof, wiskundige en Nobelprijswinnaar literatuur (1950)',
         life: '1872-1970'
     },
     {
+        id: 6,
         text: 'De mens is alleen datgene wat hij van zichzelf maakt.',
         author: 'Jean-Paul Sartre',
         info: 'Frans schrijver, filosoof en Nobelprijswinnaar literatuur (1964)',
         life: '1905-1980'
     },
     {
+        id: 7,
         text: 'Geld is als mest; het is alleen goed als het wordt verspreid.',
         author: 'Francis Bacon',
         info: 'Engels filosoof en staatsman',
         life: '1561-1626'
     },
     {
+        id: 8,
         text: 'Bankoverval: een initiatief van amateurs. De echte professionals richten een bank op.',
         author: 'Bertolt Brecht',
         info: 'Duits - Oostenrijks schrijver',
         life: '1898-1956'
     },
     {
+        id: 9,
         text: 'Wie niet beweegt, bemerkt zijn ketens niet.',
         author: 'Rosa Luxemburg',
         info: 'Duits politica en filosofe',
@@ -65,8 +75,7 @@ const authorButtonEl = document.getElementById("authorButton");
 const nextQuoteButtonEl = document.getElementById("nextQuoteButton");
 
 //state: changes on user action
-let availableQuotes = [];
-let shownQuotes = [];
+let idsOfShownQuotes = [];
 
 //pure function
 //className is a String, visible is a boolean
@@ -110,9 +119,21 @@ function makeOneAuthorDiv(quote) {
 
 //UI function
 function showAuthor() {
-    quoteEl.innerHTML += makeOneAuthorDiv(shownQuotes[0]);
+    quoteEl.innerHTML += makeOneAuthorDiv(QUOTES[idsOfShownQuotes[0]]);
     makeElementVisible(nextQuoteButtonEl, true);
     makeElementVisible(authorButtonEl, false);
+}
+
+//non-UI function, but not pure!
+function pickOneRandomQuote() {
+    if (idsOfShownQuotes.length === QUOTES.length) idsOfShownQuotes = []; // means start over
+
+    const availableQuotes = QUOTES.filter(q => !idsOfShownQuotes.includes(q.id));
+    const randomCitaatIndexInAvailableQuotes = Math.floor(Math.random() * availableQuotes.length);
+    const randomQuote = availableQuotes[randomCitaatIndexInAvailableQuotes];
+    idsOfShownQuotes = [randomQuote.id, ...idsOfShownQuotes];
+    // console.log(randomCitaatIndexInAvailableQuotes, idsOfShownQuotes);
+    return randomQuote;
 }
 
 //UI function
@@ -121,22 +142,13 @@ function makeOneQuoteDiv(quote) {
     return `<div class="card my-2 p-2"><h5>${quote.text}</h5></div>`;
 }
 
-//non-UI function, but not pure!
-function pickOneRandomQuote() {
-    if (!availableQuotes.length) availableQuotes = [...QUOTES];
-    const randomCitaatIndex = Math.floor(Math.random() * availableQuotes.length);
-    shownQuotes = [availableQuotes[randomCitaatIndex], ...shownQuotes];
-    availableQuotes = availableQuotes.toSpliced(randomCitaatIndex, 1);
-     console.log(randomCitaatIndex, shownQuotes, availableQuotes);
-}
-
 //UI function
 function showOneRandomQuote() {
-    pickOneRandomQuote();
+    const quote = pickOneRandomQuote();
 
-    quoteEl.innerHTML += makeOneQuoteDiv(shownQuotes[0]);
-    makeElementVisible(nextQuoteButtonEl, false);
-    makeElementVisible(authorButtonEl, true);
+    quoteEl.innerHTML += makeOneQuoteDiv(quote);
+    // makeElementVisible(nextQuoteButtonEl, false);
+    // makeElementVisible(authorButtonEl, true);
 }
 
 showOneRandomQuote();
